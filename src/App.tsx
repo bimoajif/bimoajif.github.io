5598188261e6bed47123b62304b4427b7e8b3ec6
bimoajif <bimoajifajrianto@gmail.com>
2023-08-07 16:20:09 +0700

    fix asset bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
     <main>
       <section className='h-[90vh] flex flex-col py-[20vh] items-center gap-10'>
         <HeaderCard />
+        <span className='asset_1'></span>
+        <span className='asset_2'></span>
         <h1 className='text-3xl font-extrabold md:w-[45%] lg:w-[45%] text-black mx-5'>Bimo is a fresh graduate IT student based in
           <span className="text-red-600">
             &nbsp;Indonesia
           </span>
         </h1>
         <p className='text-sm text-gray-500 md:w-[35%] lg:w-[35%] mx-12'>I love to design software, explore new tech, & listening music. Currently, i’m focusing on UI/UX design and mobile development using flutter.</p>
-        <span className='asset_1'></span>
-        <span className='asset_2'></span>
       </section>
       <section className='flex flex-col py-[10vh] items-center gap-12 bg-slate-50 rounded-[4em]'>
         <div className='flex flex-col items-center gap-4'>
