7bc2782b6296de61c8493fd22e9c38adcf07957a
bimoajif <bimoajifajrianto@gmail.com>
2023-08-08 10:25:33 +0700

    fix: update header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import bolt from './assets/bolt.svg'
+import bg from './assets/bg.png'
 import HeaderCard from './components/cards.header'
 import FrameworkLogo from './components/frameworklogos.skills'
 import './App.css'
@@ -7,18 +8,20 @@ function App() {
 
   return (
     <main>
-      <section className='h-[90vh] flex flex-col py-[20vh] items-center gap-10'>
-        <HeaderCard />
-        <span className='asset_1'></span>
-        <span className='asset_2'></span>
-        <h1 className='text-3xl font-extrabold md:w-[45%] lg:w-[45%] text-black mx-5'>Bimo is a fresh graduate IT student based in
+      <section className="h-[45vh] w-screen bg-cover bg-center bg-gray-400 bg-[url('./assets/IMG_9822.JPG')]">
+      </section>
+      <section className='h-[50vh] flex flex-col justify-center items-center gap-10'>
+        {/* <HeaderCard /> */}
+        {/* <span className='asset_1'></span>
+        <span className='asset_2'></span> */}
+        <h1 className='text-4xl font-extrabold md:w-[45%] lg:w-[45%] text-black mx-5'>Bimo is a fresh graduate IT student based in
           <span className="text-red-600">
-            &nbsp;Indonesia
+            &nbsp;Indonesia.
           </span>
         </h1>
         <p className='text-sm text-gray-500 md:w-[35%] lg:w-[35%] mx-12'>I love to design software, explore new tech, & listening music. Currently, i’m focusing on UI/UX design and mobile development using flutter.</p>
       </section>
-      <section className='flex flex-col py-[10vh] items-center gap-12 bg-slate-50 rounded-[4em]'>
+      <section className='flex flex-col py-[10vh] items-center gap-12 bg-slate-50 rounded-t-[3em]'>
         <div className='flex flex-col items-center gap-4'>
           <img src={bolt} className=' w-12' />
           <h1 className='text-3xl text-gray-500 font-extrabold'>
